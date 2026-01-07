0974d1def54fe3feac5c56cf0b160c8099503280
kaleimHC <kaleimhc@gmail.com>
2026-01-07 09:00:00 +0000

    W_matrix.R - contiguity + distance

--- spatialModel/R/W_matrix.R
+++ spatialModel/R/W_matrix.R
@@ -198,3 +198,203 @@ bestW<-function(points_sf, eq, model_type="SDM", sample_size, knn){
   } else {
     knn<-sort(round(knn))
   }
+
+  # sample do testowania
+  selector<-sample(nrow(points_sf), sample_size, replace=FALSE)
+  points_sf_s<-points_sf[selector,]
+  crds_s<-crds[selector, ]
+  crds_s[,1]<-crds_s[,1]+rnorm(sample_size, 0, sd(crds_s[,1])/1000)
+  crds_s[,2]<-crds_s[,2]+rnorm(sample_size, 0, sd(crds_s[,2])/1000)
+
+  # macierz rezultatów
+  result<-matrix(NA, nrow=length(knn), ncol=4)
+  colnames(result)<-c("knn", "AIC", "rho", "lambda")
+  result[,1]<-knn
+
+  if (model_type=="SAR") {
+    cat(length(knn), " ",model_type, " models will be computed. ",
+        "It can take a long while.","\n",sep="")
+    for(i in 1:length(knn)){
+      knnW_temp<-nb2listw(make.sym.nb(knn2nb(knearneigh(as.matrix(crds_s), k=knn[i]))))
+      model_temp<-lagsarlm(eq, data=points_sf_s, knnW_temp)
+      result[i,2]<-AIC(model_temp)
+      result[i,3]<-model_temp$rho
+      cat(i,"/",length(knn)," done.\n",sep="")
+    }
+
+  } else if (model_type=="SDM") {
+    cat(length(knn), " ",model_type, " models will be computed. ",
+        "It can take a long while.","\n",sep="")
+    for(i in 1:length(knn)){
+      knnW_temp<-nb2listw(make.sym.nb(knn2nb(knearneigh(as.matrix(crds_s), k=knn[i]))))
+      model_temp<-lagsarlm(eq, data=points_sf_s, knnW_temp, type="mixed")
+      result[i,2]<-AIC(model_temp)
+      result[i,3]<-model_temp$rho
+      cat(i,"/",length(knn)," done.\n",sep="")
+    }
+
+  } else if (model_type=="SEM") {
+    cat(length(knn), " ",model_type, " models will be computed. ",
+        "It can take a long while.","\n",sep="")
+    for(i in 1:length(knn)){
+      knnW_temp<-nb2listw(make.sym.nb(knn2nb(knearneigh(as.matrix(crds_s), k=knn[i]))))
+      model_temp<-errorsarlm(eq, data=points_sf_s, knnW_temp)
+      result[i,2]<-AIC(model_temp)
+      result[i,4]<-model_temp$lambda
+      cat(i,"/",length(knn)," done.\n",sep="")
+    }
+
+  } else if (model_type=="SDEM") {
+    cat(length(knn), " ",model_type, " models will be computed. ",
+        "It can take a long while.","\n",sep="")
+    for(i in 1:length(knn)){
+      knnW_temp<-nb2listw(make.sym.nb(knn2nb(knearneigh(as.matrix(crds_s), k=knn[i]))))
+      model_temp<-errorsarlm(eq, data=points_sf_s, knnW_temp, etype="emixed")
+      result[i,2]<-AIC(model_temp)
+      result[i,4]<-model_temp$lambda
+      cat(i,"/",length(knn)," done.\n",sep="")
+    }
+
+  } else if (model_type=="SAC") {
+    cat(length(knn), " ",model_type, " models will be computed. ",
+        "It can take a long while.","\n",sep="")
+    for(i in 1:length(knn)){
+      knnW_temp<-nb2listw(make.sym.nb(knn2nb(knearneigh(as.matrix(crds_s), k=knn[i]))))
+      model_temp<-sacsarlm(eq, data=points_sf_s, knnW_temp)
+      result[i,2]<-AIC(model_temp)
+      result[i,3]<-model_temp$rho
+      result[i,4]<-model_temp$lambda
+      cat(i,"/",length(knn)," done.\n",sep="")
+    }
+
+  }
+
+  result<-as.data.frame(result) # zostawiona obie kolumny rho i lambda - nieużywana w danym modelu z NA
+  best.result<-result[result$AIC==min(result$AIC),]
+  bestW.result<-nb2listw(make.sym.nb(knn2nb(knearneigh(as.matrix(crds_s), k=best.result[1]))))
+
+  # jak idzie SAR lub SDM to rho, jak SEM lub SDEM to lambda,
+  # a jak SAC to rho i lambda
+
+  # można spróbować w pętli zapamiętywać najlepszą macierz bestW.result, żeby jej nie liczyć.
+  # czy bestW.result do też jako OUTPUT? - będzie ciężko razem z df? bo trzeba to jako listę.
+
+  if (model_type=="SAR" || model_type=="SDM") {
+    par(mar=c(4,4,3,3), mfrow=c(1,2))
+    # rysunek AIC w zależności od knn
+    plot(result[,1:2], type="l", xlab="knn", ylab="AIC", lwd=2, cex.lab=0.9, cex.main=1,
+         main="AIC value for the selected model depending on knn")
+    points(result[,1:2], pch=21, bg="lightblue", cex=1.5)
+    abline(v=best.result[1], lty=3, col="grey60")
+    text(best.result[1], max(result$AIC, na.rm=TRUE), paste("best knn:", best.result[1]))
+
+    # rysunek rho w zależności od knn
+    plot(result[,c(1,3)], type="l", xlab="knn", ylab="rho", lwd=2, cex.lab=0.9, cex.main=1,
+         main="Rho for different knn")
+    points(result[,c(1,3)], pch=21, bg="coral2", cex=1.5)
+    abline(v=best.result[1], lty=3, col="grey60")
+    text(best.result[1], min(result$rho, na.rm=TRUE), "rho for best knn")
+
+  } else if (model_type=="SEM" || model_type=="SDEM") {
+    par(mar=c(4,4,3,3), mfrow=c(1,2))
+    # rysunek AIC w zależności od knn
+    plot(result[,1:2], type="l", xlab="knn", ylab="AIC", lwd=2, cex.lab=0.9, cex.main=1,
+         main="AIC value for the selected model depending on knn")
+    points(result[,1:2], pch=21, bg="lightblue", cex=1.5)
+    abline(v=best.result[1], lty=3, col="grey60")
+    text(best.result[1], max(result$AIC, na.rm=TRUE), paste("best knn:", best.result[1]))
+
+    # rysunek lambda w zależności od knn
+    plot(result[,c(1,4)], type="l", xlab="knn", ylab="lambda", lwd=2, cex.lab=0.9, cex.main=1,
+         main="Lambda for different knn")
+    points(result[,c(1,4)], pch=21, bg="seagreen3", cex=1.5)
+    abline(v=best.result[1], lty=3, col="grey60")
+    text(best.result[1], min(result$lambda, na.rm=TRUE), "lambda for best knn")
+
+  } else if (model_type=="SAC") {
+    par(mar=c(4,4,3,3), mfrow=c(1,3))
+    # rysunek AIC w zależności od knn
+    plot(result[,1:2], type="l", xlab="knn", ylab="AIC", lwd=2, cex.lab=0.9, cex.main=1,
+         main="AIC value for the selected model depending on knn")
+    points(result[,1:2], pch=21, bg="lightblue", cex=1.5)
+    abline(v=best.result[1], lty=3, col="grey60")
+    text(best.result[1], max(result$AIC, na.rm=TRUE), paste("best knn:", best.result[1]))
+
+    # rysunek rho w zależności od knn
+    plot(result[,c(1,3)], type="l", xlab="knn", ylab="rho", lwd=2, cex.lab=0.9, cex.main=1,
+         main="Rho for different knn")
+    points(result[,c(1,3)], pch=21, bg="coral2", cex=1.5)
+    abline(v=best.result[1], lty=3, col="grey60")
+    text(best.result[1], min(result$rho, na.rm=TRUE), "rho for best knn")
+
+    # rysunek lambda w zależności od knn
+    plot(result[,c(1,4)], type="l", xlab="knn", ylab="lambda", lwd=2, cex.lab=0.9, cex.main=1,
+         main="Lambda for different knn")
+    points(result[,c(1,4)], pch=21, bg="seagreen3", cex=1.5)
+    abline(v=best.result[1], lty=3, col="grey60")
+    text(best.result[1], min(result$lambda, na.rm=TRUE), "lambda for best knn")
+  }
+
+  par(mar=c(5.1,4.1,4.1,2.1), mfrow=c(1,1))
+
+  return(result)
+}
+
+#########################
+### corrSpatialLags() ###
+#########################
+#
+#' @title Empirical and theoretical correlations between spatial lags with different numbers of k nearest neighbours in the spatial weight matrix W
+#'
+#' @description
+#' Spatial lags calculated using different numbers of k nearest neighbours are correlated - strongly when the values of knn are similar,
+#' and weakly when they are different. LeSage & Pace (2014) gave the formula for theoretical correlations between these lags.
+#' The function reports two types of correlations: theoretical, as given by LeSage & Pace (2014), and empirical, using the Pearson
+#' correlation coefficient applied to real values. It can be used to select the appropriate number of k nearest neighbours to be included
+#' in spatial weights matrix for a point pattern.
+#'
+#' @details
+#' Theoretical correlation between spatial lags calculated at different nearest neighbours equals (mi/mj)^0.5 where mi<mj
+#' are the number of nearest neighbours.
+#'
+#' Empirical correlation uses Pearson correlation coefficient to determine observed correlations between spatial lags derived for
+#' the same variable and different number of k nearest neighbours.
+#'
+#' @name corrSpatialLags
+#' @param points_sf Geo-located points in `sf` or the `data.frame` class - in the case of a `data.frame` object, the first and second columns must contain X and Y coordinates.
+#' @param var_name Name of the column (as text) in `points_sf` dataset with the variable to be analysed, e.g. "variable".
+#' @param sample_size The sample size, must be less than or equal to the number of points in the dataset (`points_sf` parameter). If `sample_size` is greater, it is automatically set
+#' to the number of points in the dataset.
+#' @param knn The vector of alternative k nearest neighbours used in the subsequent spatial econometric models for the spatial weight matrix.
+#'
+#' @return `corrSpatialLags()` returns theoretical and empirical correlation matrices as numerical tables and coloured plot matrix.
+#'
+#' @references
+#' Kubara, M., & Kopczewska, K. (2023). Akaike information criterion in choosing the optimal k-nearest neighbours of the spatial weight matrix.
+#' Spatial Economic Analysis, 1-19.
+#'
+#' LeSage, J. P., & Pace, R. K. (2014). The biggest myth in spatial econometrics. Econometrics, 2(4), 217-249.
+#'
+#' @examples
+#' csl<-corrSpatialLags(firms_sf, "roa", 500, knn=1:30)
+#' csl
+#'
+#' @export
+corrSpatialLags<-function(points_sf, var_name, sample_size, knn){
+  #Ew. do sprawdzenia czy warunek st_geometry_type(data_sf,FALSE)=="POINT") nie jest zbyt restrykcyjny
+  if((inherits(points_sf,"sf") && st_geometry_type(points_sf,FALSE)=="POINT")) {
+    crds<-as.data.frame(st_coordinates(points_sf))
+    colnames(crds)<-c("X_coord","Y_coord")
+    cat("Points_sf was detected as an object of class sf.\n", sep = "")
+  }  else if(inherits(points_sf,"data.frame",TRUE)==1){
+    crds<-points_sf[,c(1,2)]
+    colnames(crds)<-c("X_coord","Y_coord")
+    cat("Points_sf was detected as an object of class data.frame.\n", sep = "")
+  }  else {
+    stop("The class of data_sf must only be 'sf' of geometry type 'POINTS' or 'data.frame'.")
+  }
+
+  # zbadać sample_size i ustawić ew. na wielkość zbioru danych (może potem zmienić, żeby )
+  sample_size <- as.integer(sample_size)
+  if (sample_size > nrow(points_sf) || sample_size<1) {
+    sample_size<-nrow(points_sf)
