d6bbb51cd1f41ba2926642593de14798e8d7873c
kaleimHC <kaleimhc@gmail.com>
2026-01-08 14:00:00 +0000

    feat(spatialModel): spatial prediction + SPAG + QDC

---
+++ spatialModel/R/qdc.R
@@ -0,0 +1,149 @@
+#############
+### QDC() ###
+#############
+#
+#
+#' @title Quick Density Clustering (QDC): identifying clusters of different densities for geolocated points
+#'
+#' @description
+#' Quick Density Clustering (QDC) uses two spatial variables - the total distance to k nearest neighbours
+#' and the number of neighbours within a fixed radius (`eps`). Both spatial variables are normalised
+#' and clustered using the K-means algorithm. Clusters separate points with different local densities.
+#' The function allows sampling to speed up the calculations.
+#'
+#' @details
+#' This algorithm is an alternative to DBSCAN. It has no problem with parameterisation, especially in the case of sampling -
+#' it gives a robust solution. It uses K-means, but unlike most algorithms - it does not cluster spatial coordinates,
+#' but pre-prepared normalised spatial variables: total distance to k nearest neighbours and number of neighbours
+#' within a fixed radius (`eps`). Its logic is simple: densely located points have a low total distance to `knn` points,
+#' while the number of points within a given radius is high. Sparsely located points, on the other hand, have a high total distance
+#' to `knn` points, while the number of nearest neighbours within a given radius is low. Both variables are self-scaling
+#' after normalisation and easily show what is high or low. K-means clustering naturally separates groups with similar values
+#' of both spatial variables. There is a non-linear relationship between the two spatial variables, visible in the output Figure 1.
+#' K-means clustering easily finds thresholds of both variables that separate clusters.
+#'
+#' The algorithm is robust to sampling. Limiting the size of the data set speeds up the computation. The parameter `sample_size` allows to find
+#' the best relation between quality and computation time.
+#'
+#' @name QDC
+#' @param data_sf Object in `sf` or the `data.frame` class - in the case of a `data.frame` object, the first and second columns must contain X and Y coordinates.
+#' @param sample_size Sample size, must be less than or equal to the number of points in the dataset (`data_sf` parameter).
+#' If `sample_size` is larger, it is automatically set to the number of points in the dataset.
+#' @param nclust Number of clusters to find. Must be >=2. For nclust=3 additional cluster labels are included in the output object.
+#' @param k Number of nearest neighbours used to calculate the total distance. The default value is k=10.
+#' @param eps Radius for counting neighbours, fixed value for all observations. The default value is eps=0.05.
+#' This is approximately 5 km for points in the WGS84 projection.
+#'
+#' @return `QDC()` returns two visualisations:
+#' * Scatter plot of both spatial variables coloured as clusters
+#' * Location of spatial points coloured according to cluster membership.
+#'
+#' `QDC()` returns also a data.frame object containing the following columns:
+#' \item{X}{original X coordinates}
+#' \item{Y}{original Y coordinates}
+#' \item{knndist1}{total distance to k nearest neighbours}
+#' \item{frnn1}{number of nearest neighbours with fixed radius}
+#' \item{knndist1.scaled}{spatial variable knndist1 after normalisation}
+#' \item{frnn1.scaled}{spatial variable frnn1 after normalisation}
+#' \item{km.set1}{cluster ID of the observation, from the K-means algorithm}
+#' \item{outcome.set1}{cluster labels, only if k=3}
+#'
+#' @references
+#' Kopczewska K., (under review), QDC: Quick Density Clustering of Geo-located Data
+#'
+#' @examples
+#' my.qdc<-QDC(firms_sf, 5000, nclust=3, k=10, eps=0.05)
+#' my.qdc
+#'
+#' @export
+QDC<-function(data_sf, sample_size, nclust=3, k=10, eps=0.05){
+  if((inherits(data_sf,"sf") && st_geometry_type(data_sf,FALSE)=="POINT")) crds<-sf::st_coordinates(data_sf)
+  else if(inherits(data_sf,"data.frame",TRUE)==1){
+    crds<-data_sf[,c(1,2)]
+    colnames(crds)<-c("X","Y")
+  }
+  else {
+    stop("The class of data_sf must only be 'sf' of geometry type 'POINTS' or 'data.frame'.")
+  }
+
+  # zbadać sample_size i ustawić ew. na wielkość zbioru danych
+  sample_size <- as.integer(sample_size)
+  if (sample_size > nrow(crds) || sample_size<1) {
+    sample_size<-nrow(crds)
+    cat("Wrong sample size. Sample_size set to:",sample_size,"\n",sep="")
+  }
+
+  # zbadać n, czy nie ujemne
+  nclust<-round(nclust,0)
+  if (nclust<=2) {
+    nclust<-3
+    cat("Wrong number of clusters. Number of cluster set to the default value: 3.","\n",sep="")
+  }
+
+  # zbadać k, czy nie ujemne
+  k<-round(k,0)
+  if (k<1) {
+    k<-10
+    cat("Wrong number of nearest neighbors. Number of nearest neighbors set to the default value: 10.","\n",sep="")
+  }
+
+  # część wspólna niezależna od nclust
+  data.d<-as.data.frame(crds[sample(nrow(crds), sample_size, replace=FALSE), ])
+
+  # spatial variable – sum of distances to knn
+  knn.dist<-kNNdist(as.matrix(data.d), k, all =TRUE)
+  data.d$knndist1<-apply(knn.dist,1,sum)
+
+  # spatial variable – number of points in fixed radius
+  agg.radius<-frNN(as.matrix(data.d), eps=0.05)
+  data.d$frnn1<-unlist(lapply(agg.radius$id, length))
+
+  # normalization of spatial variables
+  data.d$knndist1.scaled<-scale(data.d$knndist1)
+  data.d$frnn1.scaled<-scale(data.d$frnn1)
+
+  # k-means clustering of two normalized spatial variables
+  km.set1<-kmeans(data.d[ ,5:6], nclust)
+  data.d$km.set1<-as.factor(km.set1$cluster)
+
+  # etykiety dla nclust=3, może jakoś to uprościć (thresholds)
+  if (nclust==3) {
+    # thresholds
+    t1<-max(min(data.d$knndist1.scaled[data.d$km.set1==1]),
+            min(data.d$knndist1.scaled[data.d$km.set1==2]),
+            min(data.d$knndist1.scaled[data.d$km.set1==3])) # threshold
+    t1	# when knndist>t1 – it is low-density cluster
+
+    t2<-max(min(data.d$frnn1.scaled[data.d$km.set1==1]),
+            min(data.d$frnn1.scaled[data.d$km.set1==2]),
+            min(data.d$frnn1.scaled[data.d$km.set1==3]))  # threshold
+    t2	# when frnn(agg)>t2 – it is high-density cluster
+
+    # classification of points to clusters
+    # ten krok jest tylko dla 3 klastrów, bo jest przewidywalne
+    data.d$outcome.set1<-ifelse(data.d$knndist1.scaled>t1, "low-density", ifelse(data.d$frnn1.scaled>t2,"high-density", "mid-density"))
+  }
+
+
+  # visualization of clusters in 2D of spatial variables
+  p1<-ggplot(data.d, aes_string(x="knndist1.scaled", y="frnn1.scaled", color="km.set1")) +  geom_point()+ theme(legend.position="none") # xy plot of spat.var
+
+  # location of clusters in space
+  p2<-ggplot(data.d, aes_string(x="X", y="Y", color="km.set1")) +  geom_point()+ theme(legend.position="none")
+
+  grid.arrange(p1,p2, ncol = 2, nrow = 1)
+
+#  print(p1)
+#  print(p2)
+
+  # LEPSZE NAZWY KOLUMN W OUTPUCIE
+  # czy potrzeba zwracać listę parametrów i obiekt output czy tylko obiekt output wystarczy?
+  # poprawić, żeby może na ekranie było jakieś summary/table a nie lista
+  return(data.d)
+}
+
+#' @rdname QDC
+#' @export
+qdc <- QDC
+
+
---
+++ spatialModel/R/spag.R
@@ -0,0 +1,182 @@
+##############
+### SPAG() ###
+##############
+#
+#
+#' @title Spatial Agglomeration Index
+#'
+#' @description
+#' SPAG (Spatial Agglomeration Index) measures the degree of agglomeration of point data in a given area in terms of the size of the points.
+#' It is based on overlapping circles created for each point. First, the algorithm optimises the radii of the circles -
+#' the area of each circle is proportional to the size of the point, while the total area of all circles is equal to the area of the territory.
+#' Second, the algorithm calculates the union - the area covered by overlapping circles. Third, it calculates the SPAG index and its components.
+#' The coverage index is the proportion of analysed points in the total sample. The distance index is the average distance between observed points
+#' divided by the average distance between points in the regular distribution. The overlap index is the ratio of the area covered by the union of
+#' circles to the total area. SPAG is the product of all three sub-indices. SPAG values are scaled between 0 and 1. High values (ca. SPAG>0.6)
+#' represent a regular distribution, while low values of SPAG (ca. SPAG<0.1) represent strong agglomeration.
+#'
+#' @details
+#' The size of the points is important for optimising the radii of the circles. The smallest size is used as a reference and each point is given
+#' the proportional size (how many times larger) for calculations.
+#'
+#' All points in the points_sf object should be within the region_sf object. SPAG analyses the agglomeration of points in the region.
+#' The size of the region is important for the result - if `region_sf` is much larger than a spatial range of point data,
+#' SPAG detects agglomeration.
+#'
+#' @name SPAG
+#' @param points_sf Object in `sf` or the `data.frame` class - in the case of a `data.frame` object, the first and second columns must contain X and Y coordinates.
+#' For `data.frame`, make sure that the coordinates of the points are initially in the same coordinate system / projection as the `region_sf` object.
+#' All data within the object are analysed. Object in spherical projection is converted to planar projection for calculations.
+#' @param size_var The name of the variable describing the size of the analysed objects (e.g. companies), given as text, e.g. "size".
+#' @param region_sf The boundary of the area in which the `points_sf` are located, in `sf` class. Data in spherical projections are converted to planar projections for calculations.
+#'
+#' @return `SPAG()` returns a list object and a plot. The list contains five elements:
+#' \item{i.coverage}{the proportion of data analysed, in function of the default 1. More details can be found in the paper by Kopczewska et al. (2019).}
+#' \item{i.distance}{the ratio of the average empirical distance between points and the theoretical average distance between points in the regular (uniform) distribution.
+#' When points are regularly distributed, `i.distance` is approximately 1. When all points are in a single location, `i.distance` is approximately 0.}
+#' \item{i.overlap}{is the area covered by overlapping circles divided by the area of the region. If the points are regularly distributed, `i.overlap` is approximately 1.
+#' If all the points are in a single location, `i.overlap` is approximately 0.}
+#' \item{SPAG}{the spatial agglomeration index as a product of sub-indices: `i.coverage * i.distance * i.overlap`.}
+#' \item{n.obs}{the number of points included in calculations.}
+#'
+#' @references
+#' Kopczewska, K., Churski, P., Ochojski, A., & Polko, A. (2019). SPAG: Index of spatial agglomeration. Papers in Regional Science, 98(6), 2391-2424.
+#'
+#' @examples
+#' my.spag<-SPAG(firms_sf, "empl", region_sf)
+#' my.spag
+#'
+#' @export
+SPAG<-function(points_sf, size_var, region_sf){
+  if(!inherits(region_sf,"sf")) {
+    stop("The class of region_sf must only be 'sf'.\n")
+  } else if(!(st_geometry_type(region_sf,FALSE)=="MULTIPOLYGON" || st_geometry_type(region_sf,FALSE)=="POLYGON")){
+    stop("The type of region_sf must only be 'MULTIPOLYGON' or 'POLYGON'.")
+  }
+
+  #Ew. do sprawdzenia czy warunek st_geometry_type(points_sf,FALSE)=="POINT") nie jest zbyt restrykcyjny
+  if((inherits(points_sf,"sf") && st_geometry_type(points_sf,FALSE)=="POINT")) {
+    cat("Points_sf was detected as an object of class sf.\n", sep = "")
+    if (st_crs(points_sf)!=st_crs(region_sf)) {
+      # sprawdzić czy działa przekształcenie
+      points_sf<-st_transform(points_sf,crs=st_crs(region_sf))
+      cat("The coordinates for point_sf object of class type data.frame have been tranformed to a geographic coordinate
+      system / projection that matches the projection of the region_sf object: EPSG:",st_crs(region_sf)$epsg,".\n",sep="")
+    }
+  } else if(inherits(points_sf,"data.frame",TRUE)==1){
+    colnames(points_sf)[1:2]<-c("X_coord","Y_coord")
+    points_sf<-st_as_sf(points_sf,coords = c("X_coord","Y_coord"), crs=st_crs(region_sf), agr="constant")
+    cat("The coordinates from the point_sf object of class type data.frame have been assigned ",
+        "a geographic coordinate system / projection that matches the projection of the region_sf object: EPSG:", st_crs(region_sf)$epsg,".\n",
+        "The points_sf object has been converted to sf class.\n",sep="")
+  } else {
+    stop("The class of points_sf must only be 'sf' of geometry type 'POINTS' or 'data.frame'.")
+  }
+
+  # SPRAWDZIĆ CZY NIE LEPIEJ OD RAZU PRZEKSZTAŁCIĆ REGION I PUNKTY_SF DO EPSG:4326 - CHYBA W TERRA:: LEPIEJ TO DZIAŁA
+
+
+  # zbadać size_var - jeśli nie ma lub nie istnieje w danych, to pominąć, jeśli istnieje to wyciągnąć kolumnę
+  if (length(size_var)>1) {
+    size_var<-size_var[1]
+    cat("Parameter size_var longer than 1. The first element has been selected: ",size_var,"\n",sep="")
+  }
+
+  m <- match(gsub(" ", ".", size_var), colnames(points_sf))
+
+  if (is.na(m) || is.null(size_var)) {
+    stop("Unknown variable name or variable name not specified.")
+  }
+
+  points_sf_s<-points_sf[,m]
+  colnames(points_sf_s)[1]<-"size"
+  points_sf_s$size<-round(points_sf_s$size)
+
+  sizes<-unique(round(as.data.frame(points_sf[,m])[,1]))
+  firm_num<-as.data.frame(table(round(as.data.frame(points_sf[,m])[,1])))
+  colnames(firm_num)[1]<-"size"
+  firm_num$rel_size<-sizes/min(sizes)
+  firm_num$r_multipl<-sqrt(firm_num$rel_size)
+
+  area_region<-st_area(region_sf) #denominator of nomin.coverage.sel
+
+  f.char<-character()
+  f.all<-character()
+  for(i in 1:nrow(firm_num)){
+    f.char<-paste(f.char,paste("firm_num[",i,",2]*pi*firm_num[",i,",3]*r^2",sep=''),sep='+')
+  }
+  eval(parse(text = paste('f.all <- function(r) { return(' , f.char , '-as.numeric(area_region))}', sep='')))
+
+  res_root<-uniroot(f.all, c(0, 100000), tol = 1e-35)
+
+  points_sf_s<-merge(points_sf_s,firm_num[,c(1,4)],by="size")
+  points_sf_s$rad<-points_sf_s$r_multipl*res_root$root
+  points_sf_s$area<-pi*(points_sf_s$rad)^2
+
+  points_vec_s<-vect(points_sf_s)
+  circles_vec_s<-buffer(points_vec_s, width=points_vec_s$rad) # single circles
+  circles_vec_s<-aggregate(circles_vec_s) #merged circles
+
+  # random matrix of distances
+  # MOŻE 3000 UZALEŻNIĆ OD WIELKOŚCI OBSZARU - JAKOŚ PROPORCJONALNIE
+  selector_rnd<-sample(1:nrow(points_sf), min(nrow(points_sf_s),3000), replace = FALSE)
+  points_sf_rnd_crds<-st_coordinates(points_sf_s[selector_rnd,]) # any random points from the sample
+  distance_rnd<-dist(points_sf_rnd_crds)
+
+  # counters for analysed sector
+  counter_distance<-mean(distance_rnd)
+  counter_coverage<-sum(points_sf_s$area)
+  counter_overlap<-expanse(circles_vec_s)
+
+  # theoretical part
+  # equal location of selected points
+  # we use object ‘points_sf_theo’ with uniform distribution
+
+  points_sf_theo<-st_sample(region_sf, 5000, type="regular")
+  st_crs(points_sf_theo)<-st_crs(region_sf)
+  # points_sf_theo<-st_transform(points_sf_theo, 4326) 	# NA RAZIE BEZ KONWERSJI - spr. konwersja do WGS84 (bo lepiej działa)
+
+  theo_rad<-sqrt(area_region/(length(points_sf_theo)*pi))
+
+  # distances for selected theoretical locations   random selection
+  # CZY 100 jako min. tu wystarczy??? Inaczej było w empirycznym???
+  selector_theo<-sample(1:length(points_sf_theo), min(100, length(points_sf_theo)), replace=FALSE)
+  points_sf_theo_rnd_crds<-st_coordinates(points_sf_theo[selector_theo])
+  distance_theo<-dist(points_sf_theo_rnd_crds)
+
+  nomin_distance<-mean(distance_theo)
+  nomin_coverage<-area_region
+  nomin_overlap<-length(points_sf_theo)*pi*theo_rad^2 #in fact area of region
+
+  # CZY nomin_coverage = nomin_overlap = area_region ???? SPRAWDZIĆ???
+
+  #SPAG
+  i_coverage<-as.numeric(counter_coverage/nomin_coverage)
+  i_distance<-as.numeric(counter_distance/nomin_distance)
+  i_overlap<-as.numeric(counter_overlap/nomin_overlap)
+  SPAG_res<- i_coverage * i_distance * i_overlap
+
+  # MOŻE PROBLEM Z LEGENDĄ - sprawdzić
+  par(mar=c(2,2,2,2)+0.1)
+  plot(st_geometry(region_sf))
+  plot(circles_vec_s, col="lightblue", add=TRUE)
+  legend("bottomleft", legend=c(paste("i.coverage=",round(i_coverage,2)),
+                                paste("i.distance=",round(i_distance,2)),
+                                paste("i.overlap=",round(i_overlap,2)),
+                                paste("SPAG=",round(SPAG_res,3)),
+                                paste("n obs.=",round(nrow(points_sf_s),2))), cex=0.85, bty="n")
+
+  title(main="SPAG measure for the analysed data", cex.main=0.9)
+
+  list(i.coverage = i_coverage,
+       i.distance = i_distance,
+       i.overlap = i_overlap,
+       SPAG = SPAG_res,
+       n.obs = nrow(points_sf_s))
+}
+
+#' @rdname SPAG
+#' @export
+spag <- SPAG
+
+
